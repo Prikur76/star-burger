737827066da35454ff8d7660a196793623ea8c4f
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-12 12:25:47 +0300

    Load data with management commands

---
+++ foodcartapp/management/commands/load_fixtures.py
@@ -0,0 +1,15 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Load fixtures"
+
+    def handle(self, *args, **options):
+        call_command("makemigrations")
+        call_command("migrate")
+        call_command("loaddata", "db_restaurants_fixture.json")
+        call_command("loaddata", "db_productcategory_fixture.json")
+        call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_restaurantmenuitem_fixture.json")
+        call_command("loaddata", "db_auth_user_fixture.json")
