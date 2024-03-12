2111ef626ef457837b9b43baa87876d65a40e031
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-12 13:43:45 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.http import JsonResponse
 from django.templatetags.static import static
@@ -63,7 +64,81 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     form_content = request.data
-    # print(form_content)
+
+    # Firstname field
+    if 'firstname' not in form_content:
+        return Response(
+            data={'error': 'the firstname field is empty or not represented'},
+            status=400
+        )
+    if not form_content['firstname']:
+        return Response(
+            data={'error': 'firstname key empty or not presented'},
+            status=400
+        )
+    if not isinstance(form_content['firstname'], str):
+        return Response(
+            data={'error': 'wrong type of the firstname field'},
+            status=400
+        )
+
+    # Lastname field
+    if 'lastname' not in form_content:
+        return Response(
+            data={'error': 'the lastname field is empty or not represented'},
+            status=400
+        )
+    if not form_content['lastname']:
+        return Response(
+            data={'error': 'lastname key empty or not presented'},
+            status=400
+        )
+    if not isinstance(form_content['lastname'], str):
+        return Response(
+            data={'error': 'wrong type of the lastname field'},
+            status=400
+        )
+
+    # Phonenumber field
+    if 'phonenumber' not in form_content:
+        return Response(
+            data={'error': 'the phonenumber field is empty or not represented'},
+            status=400
+        )
+    if not form_content['phonenumber']:
+        return Response(
+            data={'error': 'phonenumber key empty or not presented'},
+            status=400
+        )
+    if not isinstance(form_content['phonenumber'], str):
+        return Response(
+            data={'error': 'wrong type of the phonenumber field'},
+            status=400
+        )
+    if not bool(re.match(r'^\+79|79|89\d{9}$', form_content['phonenumber'])):
+        return Response(
+            data={'error': 'wrong type of the phonenumber field'},
+            status=400
+        )
+
+    # Address field
+    if 'address' not in form_content:
+        return Response(
+            data={'error': 'the address field is empty or not represented'},
+            status=400
+        )
+    if not form_content['address']:
+        return Response(
+            data={'error': 'address key empty or not presented'},
+            status=400
+        )
+    if not isinstance(form_content['address'], str):
+        return Response(
+            data={'error': 'wrong type of the address field'},
+            status=400
+        )
+
+    # Products field
 
     if 'products' not in form_content:
         return Response(
@@ -80,6 +155,35 @@ def register_order(request):
             data={'error': 'wrong type of the products field'},
             status=400
         )
+    products = Product.objects.all()
+    for product in form_content['products']:
+        if 'product' not in product:
+            return Response(
+                data={'error': 'the product field is empty or not represented'},
+                status=400
+            )
+        if 'quantity' not in product:
+            return Response(
+                data={'error': 'the quantity field is empty or not represented'},
+                status=400
+            )
+
+        if not isinstance(product['product'], int):
+            return Response(
+                data={'error': 'wrong type of the product field'},
+                status=400
+            )
+        if not isinstance(product['quantity'], int):
+            return Response(
+                data={'error': 'wrong type of the quantity field'},
+                status=400
+            )
+
+        if not products.filter(id=product['product']).exists():
+            return Response(
+                data={'error': 'wrong product id'},
+                status=400
+            )
 
     order = Order.objects.get_or_create(
         first_name=form_content.get('firstname'),
@@ -87,7 +191,7 @@ def register_order(request):
         phonenumber=form_content.get('phonenumber'),
         address=form_content.get('address')
     )[0]
-    products = Product.objects.all()
+
     for product in form_content.get('products'):
         order_item = OrderItem.objects.create(
             order=order,
