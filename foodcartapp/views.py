50ed63a525b696cfee235c94414bc207e28fb9cf
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-12 18:05:14 +0300

    Add validate request

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -40,7 +40,7 @@ def banners_list_api(request):
     })
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def product_list_api(request):
     products = Product.objects.select_related('category').available()
 
@@ -66,145 +66,160 @@ def product_list_api(request):
     return Response(dumped_products)
 
 
-def validate_order(request):
-    pass
-
-
-
 @api_view(['POST'])
 def register_order(request):
-    form_content = request.data
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    # Firstname field
-    if 'firstname' not in form_content:
-        return Response(
-            data={'error': 'the firstname field is empty or not represented'},
-            status=400
-        )
-    if not form_content['firstname']:
-        return Response(
-            data={'error': 'firstname key empty or not presented'},
-            status=400
-        )
-    if not isinstance(form_content['firstname'], str):
-        return Response(
-            data={'error': 'wrong type of the firstname field'},
-            status=400
-        )
+    order = Order.objects.create(
+        firstname=serializer.validated_data['firstname'],
+        lastname=serializer.validated_data['lastname'],
+        phonenumber=serializer.validated_data['phonenumber'],
+        address=serializer.validated_data['address']
+    )
+    products_fields = serializer.validated_data['products']
+    products = [
+        OrderItem(order=order, product=product['product'], quantity=product['quantity'])
+        for product in products_fields]
+    OrderItem.objects.bulk_create(products)
 
-    # Lastname field
-    if 'lastname' not in form_content:
-        return Response(
-            data={'error': 'the lastname field is empty or not represented'},
-            status=400
-        )
-    if not form_content['lastname']:
-        return Response(
-            data={'error': 'lastname key empty or not presented'},
-            status=400
-        )
-    if not isinstance(form_content['lastname'], str):
-        return Response(
-            data={'error': 'wrong type of the lastname field'},
-            status=400
-        )
+    return Response({'id': order.id})
 
-    # Phonenumber field
-    if 'phonenumber' not in form_content:
-        return Response(
-            data={'error': 'the phonenumber field is empty or not represented'},
-            status=400
-        )
-    if not form_content['phonenumber']:
-        return Response(
-            data={'error': 'phonenumber key empty or not presented'},
-            status=400
-        )
-    if not isinstance(form_content['phonenumber'], str):
-        return Response(
-            data={'error': 'wrong type of the phonenumber field'},
-            status=400
-        )
-    if not bool(re.match(r'^\+79|79|89\d{9}$', form_content['phonenumber'])):
-        return Response(
-            data={'error': 'wrong type of the phonenumber field'},
-            status=400
-        )
 
-    # Address field
-    if 'address' not in form_content:
-        return Response(
-            data={'error': 'the address field is empty or not represented'},
-            status=400
-        )
-    if not form_content['address']:
-        return Response(
-            data={'error': 'address key empty or not presented'},
-            status=400
-        )
-    if not isinstance(form_content['address'], str):
-        return Response(
-            data={'error': 'wrong type of the address field'},
-            status=400
-        )
-    # Products field
-    if 'products' not in form_content:
-        return Response(
-                data={'error': 'the products field is empty or not represented'},
-                status=400
-        )
-    if not form_content['products']:
-        return Response(
-            data={'error': 'the products field is empty or not represented'},
-            status=400
-        )
-    if not isinstance(form_content['products'], list):
-        return Response(
-            data={'error': 'wrong type of the products field'},
-            status=400
-        )
-    products = Product.objects.all()
-    for product in form_content['products']:
-        if 'product' not in product:
-            return Response(
-                data={'error': 'the product field is empty or not represented'},
-                status=400
-            )
-        if 'quantity' not in product:
-            return Response(
-                data={'error': 'the quantity field is empty or not represented'},
-                status=400
-            )
-
-        if not isinstance(product['product'], int):
-            return Response(
-                data={'error': 'wrong type of the product field'},
-                status=400
-            )
-        if not isinstance(product['quantity'], int):
-            return Response(
-                data={'error': 'wrong type of the quantity field'},
-                status=400
-            )
-
-        if not products.filter(id=product['product']).exists():
-            return Response(
-                data={'error': 'wrong product id'},
-                status=400
-            )
-
-    order = Order.objects.get_or_create(
-        first_name=form_content.get('firstname'),
-        last_name=form_content.get('lastname'),
-        phonenumber=form_content.get('phonenumber'),
-        address=form_content.get('address')
-    )[0]
-
-    for product in form_content.get('products'):
-        order_item = OrderItem.objects.create(
-            order=order,
-            product=products.get(id=product.get('product')),
-            quantity=product.get('quantity')
-        )
-
-    return Response({'order_id': order.id}, status=200)
+# @api_view(['POST'])
+# def register_order(request):
+#     form_content = request.data
+#
+#     # Firstname field
+#     if 'firstname' not in form_content:
+#         return Response(
+#             data={'error': 'the firstname field is empty or not represented'},
+#             status=400
+#         )
+#     if not form_content['firstname']:
+#         return Response(
+#             data={'error': 'firstname key empty or not presented'},
+#             status=400
+#         )
+#     if not isinstance(form_content['firstname'], str):
+#         return Response(
+#             data={'error': 'wrong type of the firstname field'},
+#             status=400
+#         )
+#
+#     # Lastname field
+#     if 'lastname' not in form_content:
+#         return Response(
+#             data={'error': 'the lastname field is empty or not represented'},
+#             status=400
+#         )
+#     if not form_content['lastname']:
+#         return Response(
+#             data={'error': 'lastname key empty or not presented'},
+#             status=400
+#         )
+#     if not isinstance(form_content['lastname'], str):
+#         return Response(
+#             data={'error': 'wrong type of the lastname field'},
+#             status=400
+#         )
+#
+#     # Phonenumber field
+#     if 'phonenumber' not in form_content:
+#         return Response(
+#             data={'error': 'the phonenumber field is empty or not represented'},
+#             status=400
+#         )
+#     if not form_content['phonenumber']:
+#         return Response(
+#             data={'error': 'phonenumber key empty or not presented'},
+#             status=400
+#         )
+#     if not isinstance(form_content['phonenumber'], str):
+#         return Response(
+#             data={'error': 'wrong type of the phonenumber field'},
+#             status=400
+#         )
+#     if not bool(re.match(r'^\+79|79|89\d{9}$', form_content['phonenumber'])):
+#         return Response(
+#             data={'error': 'wrong type of the phonenumber field'},
+#             status=400
+#         )
+#
+#     # Address field
+#     if 'address' not in form_content:
+#         return Response(
+#             data={'error': 'the address field is empty or not represented'},
+#             status=400
+#         )
+#     if not form_content['address']:
+#         return Response(
+#             data={'error': 'address key empty or not presented'},
+#             status=400
+#         )
+#     if not isinstance(form_content['address'], str):
+#         return Response(
+#             data={'error': 'wrong type of the address field'},
+#             status=400
+#         )
+#     # Products field
+#     if 'products' not in form_content:
+#         return Response(
+#                 data={'error': 'the products field is empty or not represented'},
+#                 status=400
+#         )
+#     if not form_content['products']:
+#         return Response(
+#             data={'error': 'the products field is empty or not represented'},
+#             status=400
+#         )
+#     if not isinstance(form_content['products'], list):
+#         return Response(
+#             data={'error': 'wrong type of the products field'},
+#             status=400
+#         )
+#     products = Product.objects.all()
+#     for product in form_content['products']:
+#         if 'product' not in product:
+#             return Response(
+#                 data={'error': 'the product field is empty or not represented'},
+#                 status=400
+#             )
+#         if 'quantity' not in product:
+#             return Response(
+#                 data={'error': 'the quantity field is empty or not represented'},
+#                 status=400
+#             )
+#
+#         if not isinstance(product['product'], int):
+#             return Response(
+#                 data={'error': 'wrong type of the product field'},
+#                 status=400
+#             )
+#         if not isinstance(product['quantity'], int):
+#             return Response(
+#                 data={'error': 'wrong type of the quantity field'},
+#                 status=400
+#             )
+#
+#         if not products.filter(id=product['product']).exists():
+#             return Response(
+#                 data={'error': 'wrong product id'},
+#                 status=400
+#             )
+#
+#     order = Order.objects.get_or_create(
+#         first_name=form_content.get('firstname'),
+#         last_name=form_content.get('lastname'),
+#         phonenumber=form_content.get('phonenumber'),
+#         address=form_content.get('address')
+#     )[0]
+#
+#     for product in form_content.get('products'):
+#         order_item = OrderItem.objects.create(
+#             order=order,
+#             product=products.get(id=product.get('product')),
+#             quantity=product.get('quantity')
+#         )
+#
+#     return Response({'order_id': order.id}, status=200)
