620aa7f4ea7e6f9be047de8bf20169635f2b8c4c
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-12 17:25:24 +0300

    Fix fields in foodcartapp/models.py

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -170,7 +170,7 @@ class OrderItem(models.Model):
         related_name='items',
         verbose_name='заказ',
     )
-    product = models.ForeignKey(
+    products = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
         related_name='items',
