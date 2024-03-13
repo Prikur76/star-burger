9a5120d0de7db29d5484c400c4a5906d506c410b
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-13 16:23:02 +0300

    Fix models.py

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -160,7 +160,7 @@ class Order(models.Model):
         verbose_name_plural = 'заказы'
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.address}"
+        return f"{self.firstname} {self.lastname} - {self.address}"
 
 
 class OrderItem(models.Model):
