de3390453a04ef570527926a4dd9d7930fcd2790
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-21 18:15:31 +0300

    Add calculate distance in orders (geocoder.py)

--- foodcartapp/api/geocoder.py
+++ foodcartapp/api/geocoder.py
@@ -1,4 +1,5 @@
 import requests
+from geopy.distance import distance
 
 
 def fetch_coordinates(apikey, address):
@@ -20,3 +21,20 @@ def fetch_coordinates(apikey, address):
     most_relevant = found_places[0]
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
     return lon, lat
+
+
+def get_distance_km(apikey, orders):
+    """
+    Returns distance between order and restaurants
+    """
+    for order in orders:
+        order_lon, order_lat = fetch_coordinates(apikey, order.get('address'))
+        if not order.get('available_restaurants'):
+            return orders
+        for restaurant in order.get('available_restaurants'):
+            if restaurant.get('address'):
+                restaurant_lon, restaurant_lat = fetch_coordinates(apikey, restaurant.get('address'))
+            if order_lat and order_lon and restaurant_lat and restaurant_lon:
+                restaurant['distance'] = round(distance((order_lat, order_lon), (restaurant_lat, restaurant_lon)).km,2)
+        order['available_restaurants'] = sorted(order.get('available_restaurants'), key=lambda x: x['distance'])
+    return orders
