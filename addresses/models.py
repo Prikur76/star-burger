7fedd072e4a85d9698a5fdea3f3149fd24ff33b0
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-25 18:26:11 +0300

    Fix Place model

--- addresses/models.py
+++ addresses/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Place(models.Model):
     address = models.CharField(unique=True, max_length=255, verbose_name='aдрес')
-    latitude = models.FloatField(verbose_name='широта', default=0.0)
     longitude = models.FloatField(verbose_name='долгота', default=0.0)
+    latitude = models.FloatField(verbose_name='широта', default=0.0)
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='дата обновления', auto_now=True)
 
@@ -13,4 +13,4 @@ class Place(models.Model):
         verbose_name_plural = 'адреса'
 
     def __str__(self):
-        return f"{self.address} ({self.latitude}, {self.longitude})"
+        return f"{self.address} ({self.longitude}, {self.latitude})"
